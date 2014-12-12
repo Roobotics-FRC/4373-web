d445e2f2784877ca700a25626f9ea966f86c6ed1
Henry Pitcairn <735Tesla@gmail.com>
2014-12-12 13:25:41 -0500

    done homepage

--- 4373roobotics/app/views/home.blade.php
+++ 4373roobotics/app/views/home.blade.php
@@ -12,11 +12,18 @@
 <!-- <img src="{{ asset('images/header2.jpg') }}" style="width:100%;padding:0px;pading-left:0px;position:fixed;"> -->
 </div>
 <div class="about">
-
+<p>&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor, leo pretium sodales molestie, turpis ligula sodales purus, et faucibus nisl lacus sed justo. Donec tempor aliquam leo, at ornare est blandit vitae. Integer sit amet sollicitudin erat, nec interdum nibh. Praesent interdum consectetur lectus vitae euismod. Suspendisse at accumsan libero. Integer tempor euismod dolor, sit amet porttitor orci malesuada sit amet. Nunc eget enim est. Sed egestas venenatis egestas. Proin laoreet dolor in ante ullamcorper placerat. Morbi pretium quam ex, a tristique urna varius ut.
+<br><hr><br>
+&nbsp;&nbsp;&nbsp;&nbsp;Aliquam vestibulum lacus ultrices enim consectetur egestas. Praesent mollis nisl libero, vitae cursus velit malesuada finibus. Phasellus in fermentum metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc at sapien diam. Donec vel urna luctus tellus pellentesque vulputate. Nullam mollis in ipsum ac dapibus. Duis at tellus quis lorem sollicitudin viverra at ut felis. Nulla eu lorem risus. Mauris consectetur, mauris ac congue rutrum, mi leo accumsan ante, elementum viverra lectus nisl sed ante. Pellentesque vel dui non turpis finibus pretium.
+</p>
 </div>
 <br><br><br><br><br>
 <div class="about">
-<h4>fuck this</h4>
+<p>
+&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor, leo pretium sodales molestie, turpis ligula sodales purus, et faucibus nisl lacus sed justo. Donec tempor aliquam leo, at ornare est blandit vitae. Integer sit amet sollicitudin erat, nec interdum nibh. Praesent interdum consectetur lectus vitae euismod. Suspendisse at accumsan libero. Integer tempor euismod dolor, sit amet porttitor orci malesuada sit amet. Nunc eget enim est. Sed egestas venenatis egestas. Proin laoreet dolor in ante ullamcorper placerat. Morbi pretium quam ex, a tristique urna varius ut.
+<br><hr><br>
+&nbsp;&nbsp;&nbsp;&nbsp;Aliquam vestibulum lacus ultrices enim consectetur egestas. Praesent mollis nisl libero, vitae cursus velit malesuada finibus. Phasellus in fermentum metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc at sapien diam. Donec vel urna luctus tellus pellentesque vulputate. Nullam mollis in ipsum ac dapibus. Duis at tellus quis lorem sollicitudin viverra at ut felis. Nulla eu lorem risus. Mauris consectetur, mauris ac congue rutrum, mi leo accumsan ante, elementum viverra lectus nisl sed ante. Pellentesque vel dui non turpis finibus pretium.
+</p>
 </div>
 <br><br><br><br><br>
 @stop
