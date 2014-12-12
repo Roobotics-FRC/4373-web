b932e936b5e0e95d625b02e5818f1f3be00ce545
Henry Pitcairn <735Tesla@gmail.com>
2014-12-12 11:10:29 -0500

    sexy

--- 4373roobotics/app/views/home.blade.php
+++ 4373roobotics/app/views/home.blade.php
@@ -1,13 +1,15 @@
 @extends('layout')
 
 @section('head')
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
 <title>Team 4373</title>
 @stop
 
 @section('content')
 <!-- <p>hello world</p> -->
-<div id="header-wrapper">
-<img src="{{ asset('images/header2.jpg') }}" style="width:100%;padding:0px;pading-left:0px;position:fixed;">
+<div id="background">
+
+<!-- <img src="{{ asset('images/header2.jpg') }}" style="width:100%;padding:0px;pading-left:0px;position:fixed;"> -->
 </div>
 @stop
 
