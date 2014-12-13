725b4848eb5c8cdb494dcb5cea217d1c4d8c57b4
Henry Pitcairn <735Tesla@gmail.com>
2014-12-13 18:31:49 -0500

    credits page / begin user backend

--- 4373roobotics/public/js/sidebar.js
+++ 4373roobotics/public/js/sidebar.js
@@ -2,7 +2,7 @@ var toggled = false;
 	$("#wrapper").toggleClass("toggled");
 	$(document).ready(function() {
 		$(document).mousemove(function(event) {
-			if (event.pageX < 175) {
+			if (event.pageX < 130) {
 				if (!toggled) {
 					$("#wrapper").toggleClass("toggled");
 					toggled = true;
