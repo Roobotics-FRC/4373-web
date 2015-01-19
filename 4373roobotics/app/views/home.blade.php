ca14d2f6533b9e527aefcd3045156e116535e0bd
735tesla <henryjamespit@gmail.com>
2015-01-19 14:48:48 +0000

    more css

--- 4373roobotics/app/views/home.blade.php
+++ 4373roobotics/app/views/home.blade.php
@@ -15,14 +15,16 @@
 <!-- <img src="{{ asset('images/header2.jpg') }}" style="width:100%;padding:0px;pading-left:0px;position:fixed;"> -->
 </div>
 <div class="about">
-<h5>Robotics at AFS</h5>
-<br>
-&nbsp;&nbsp;&nbsp;&nbsp;On any day of the week you’ll find a lively throng of students brainstorming, coding and building in the Upper School robotics lab. “Students pick up a lot of valuable skills here, above and beyond what they learn in the classroom,” says Upper School Math, Science and Engineering Teacher John Silvers.  The hands-on, practical skill building involved in designing and creating a robot engages students on multiple levels. Working together as a team they learn to problem solve, take risks, bounce back from failure and reach for creative, innovative solutions. While developing these vital 21st- century skills, they’re also having a whole lot of fun!
+<h3>Robotics at <a class="bodylink" href="http://www.abingtonfriends.net/Home/Home">AFS</a></h3>
+<p class="bodytext">
+On any day of the week you’ll find a lively throng of students brainstorming, coding and building in the Upper School robotics lab. “Students pick up a lot of valuable skills here, above and beyond what they learn in the classroom,” says Upper School Math, Science and Engineering Teacher John Silvers.  The hands-on, practical skill building involved in designing and creating a robot engages students on multiple levels. Working together as a team they learn to problem solve, take risks, bounce back from failure and reach for creative, innovative solutions. While developing these vital 21st- century skills, they’re also having a whole lot of fun!
+</p>
 
-<br><hr><br>
-<h5>FIRST Robotics Competition</h5>
-&nbsp;&nbsp;&nbsp;&nbsp;For the last three years the AFS Robotics Team has taken part in the FIRST Robotics Competition, an international high school robotics competition. Each January a new challenge (design a robot that can shoot hoops, launch Frisbees, etc) is delivered to all teams, along with a standardized kit of motors, parts and computer components. After six weeks of intense problem solving, prototyping, writing code and building, the teams face off in district and regional events. In its first year of competition, our team scored the most points of any rookie team. Last year, they advanced to the regionals and won the Industrial Design Award.
-
+<br><hr>
+<h3>FIRST Robotics Competition</h3>
+<p class="bodytext">
+For the last three years the AFS Robotics Team has taken part in the FIRST Robotics Competition, an international high school robotics competition. Each January a new challenge (design a robot that can shoot hoops, launch Frisbees, etc) is delivered to all teams, along with a standardized kit of motors, parts and computer components. After six weeks of intense problem solving, prototyping, writing code and building, the teams face off in district and regional events. In its first year of competition, our team scored the most points of any rookie team. Last year, they advanced to the regionals and won the Industrial Design Award.
+</p>
 </div>
 <br><br><br><br><br>
 @stop
